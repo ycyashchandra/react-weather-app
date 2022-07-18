97c429705cd27d9e61a05365ea9443236aadea36
ycyashchandra <ycyashchandra1511@outlook.com>
2022-07-18 17:57:49 +0530

    First Commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '321330d6efmsh82c40ee856d4bddp13a1d5jsn25c6c8123f64',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_KEY = "13db66d4cc03878b7978dff45aa2d2b6"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
